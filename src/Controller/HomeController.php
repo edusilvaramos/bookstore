726b903f227c0338821209c800f4fbb5ec643a48
edusilvaramos <edusilvaramos@hotmail.com>
2026-02-19 11:10:27 +0100

    fix booc x home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/book')]
-final class BookController extends AbstractController
+#[Route('/home')]
+final class HomeController extends AbstractController
 {
-    #[Route(name: 'app_book_index', methods: ['GET'])]
+    #[Route(name: 'app_home_index', methods: ['GET'])]
     public function index(BookRepository $bookRepository): Response
     {
         return $this->render('book/index.html.twig', [
@@ -22,7 +22,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_book_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_home_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $book = new Book();
@@ -33,7 +33,7 @@ final class BookController extends AbstractController
             $entityManager->persist($book);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('book/new.html.twig', [
@@ -42,7 +42,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_home_show', methods: ['GET'])]
     public function show(Book $book): Response
     {
         return $this->render('book/show.html.twig', [
@@ -50,7 +50,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_book_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_home_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Book $book, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(BookType::class, $book);
@@ -59,7 +59,7 @@ final class BookController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('book/edit.html.twig', [
@@ -68,7 +68,7 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_home_delete', methods: ['POST'])]
     public function delete(Request $request, Book $book, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->getPayload()->getString('_token'))) {
@@ -76,6 +76,6 @@ final class BookController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
     }
 }
